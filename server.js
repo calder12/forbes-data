4064c7c1a9743aa7d94b3bbf23a1b34dca7888b8
Rick Calder <rick@calder.io>
2019-01-24 15:08:25 -0500

    change server port back

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const express = require('express')
 const request = require('request')
 const app = express()
-const PORT = process.env.PORT || 8443
+const PORT = process.env.PORT || 7777
 
 
 // create application/json parser
@@ -19,7 +19,7 @@ app.use(urlencodedParser);
 app.use(jsonParser);
 app.use(express.static(__dirname + "/app/build/"));
 
-app.get('/data', function (req, res) {
+app.get('/', function (req, res) {
   request('https://www.forbes.com/ajax/list/data?year=2018&uri=billionaires&type=person', function(error, response, body) {
     if (error) {
       throw error;
